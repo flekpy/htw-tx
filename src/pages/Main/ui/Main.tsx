b5ac06fe0c7edd1aa6dc6432da30bc8bb45b43b6
flekpy <flekpy@gmail.com>
2023-10-26 11:03:20 +0800

    add eth logo 3d model

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,8 +1,10 @@
+import { Eth3dLogo } from 'shared/ui/Eth3dModel'
 import { CreateTx } from 'features/CreateTx'
 
 export const Main = () => {
   return (
     <>
+      <Eth3dLogo />
       <CreateTx />
     </>
   )
